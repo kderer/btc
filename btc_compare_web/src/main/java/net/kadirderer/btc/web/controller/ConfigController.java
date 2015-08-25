05dc66b1f90cb46920e869d0467c2fa9d2bf15bc
kderer <kderer@hotmail.com>
2015-08-26 00:38:42 +0800

    Config Map controller

--- btc_compare_web/src/main/java/net/kadirderer/btc/web/controller/ConfigController.java
+++ btc_compare_web/src/main/java/net/kadirderer/btc/web/controller/ConfigController.java
@@ -23,25 +23,25 @@ public class ConfigController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String index(ModelMap model) {
 		
-//		Map<String, String> configValueMap = configService.receiveConfigValues();
-//		
-//		model.addAttribute("recordgroupjob_enabled", configValueMap.get("recordgroupjob_enabled"));		
-//		model.addAttribute("autotrade_enabled", configValueMap.get("autotrade_enabled"));
-//		model.addAttribute("sellorder_check_period", configValueMap.get("sellorder_check_period"));
-//		model.addAttribute("sellorder_delta", configValueMap.get("sellorder_delta"));
-//		model.addAttribute("sellorder_time_limit", configValueMap.get("sellorder_time_limit"));
-//		model.addAttribute("buyorder_time_limit", configValueMap.get("buyorder_time_limit"));
-//		model.addAttribute("buyorder_delta", configValueMap.get("buyorder_delta"));
-//		model.addAttribute("buyorder_check_period", configValueMap.get("buyorder_check_period"));
+		Map<String, String> configValueMap = configService.receiveConfigValues();
 		
-		model.addAttribute("recordgroupjob_enabled", "true");		
-		model.addAttribute("autotrade_enabled", "ture");
-		model.addAttribute("sellorder_check_period", "ture");
-		model.addAttribute("sellorder_delta", "ture");
-		model.addAttribute("sellorder_time_limit", "ture");
-		model.addAttribute("buyorder_time_limit", "ture");
-		model.addAttribute("buyorder_delta", "ture");
-		model.addAttribute("buyorder_check_period", "ture");
+		model.addAttribute("recordgroupjob_enabled", configValueMap.get("recordgroupjob_enabled"));		
+		model.addAttribute("autotrade_enabled", configValueMap.get("autotrade_enabled"));
+		model.addAttribute("sellorder_check_period", configValueMap.get("sellorder_check_period"));
+		model.addAttribute("sellorder_delta", configValueMap.get("sellorder_delta"));
+		model.addAttribute("sellorder_time_limit", configValueMap.get("sellorder_time_limit"));
+		model.addAttribute("buyorder_time_limit", configValueMap.get("buyorder_time_limit"));
+		model.addAttribute("buyorder_delta", configValueMap.get("buyorder_delta"));
+		model.addAttribute("buyorder_check_period", configValueMap.get("buyorder_check_period"));
+		
+//		model.addAttribute("recordgroupjob_enabled", "true");		
+//		model.addAttribute("autotrade_enabled", "ture");
+//		model.addAttribute("sellorder_check_period", "ture");
+//		model.addAttribute("sellorder_delta", "ture");
+//		model.addAttribute("sellorder_time_limit", "ture");
+//		model.addAttribute("buyorder_time_limit", "ture");
+//		model.addAttribute("buyorder_delta", "ture");
+//		model.addAttribute("buyorder_check_period", "ture");
 		
 		return "config/list_config";
 	}
